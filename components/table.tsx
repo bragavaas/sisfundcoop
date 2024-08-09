29fd869eb0ecc07427699e8642191d9ed5a7124c
andre <bragavaas@gmail.com>
2024-08-09 18:03:07 -0300

    table component

--- components/table.tsx
+++ components/table.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React from "react";
-import {Table as NextTable, TableHeader, TableColumn, TableBody, TableRow, TableCell, Button} from "@nextui-org/react";
+import { Table as NextTable, TableHeader, TableColumn, TableBody, TableRow, TableCell, Button } from "@nextui-org/react";
 
 // Define types for the props
 type TableHeaderType = {
@@ -10,7 +10,7 @@ type TableHeaderType = {
 
 type TableRowType = {
   key: string;
-  cells: React.ReactNode[];
+  cells: React.ReactNode[];  // Ensure this is compatible with React elements
 };
 
 interface TableProps {
@@ -20,25 +20,25 @@ interface TableProps {
 
 const Table: React.FC<TableProps> = ({ headers, rows }) => {
   return (
-      <NextTable aria-label="Example static collection table">
-        <TableHeader>
-          {headers.map((header) => (
-            <TableColumn key={header.key}>{header.label}</TableColumn>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.key}>
-              {row.cells.map((cell, index) => (
-                <TableCell key={index}>{cell}</TableCell>
-              ))}
-               <TableCell key="action">
-                <Button size="sm">Visualizar</Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </NextTable>
+    <NextTable aria-label="Example static collection table">
+      <TableHeader>
+        {headers.map((header) => (
+          <TableColumn key={header.key}>{header.label}</TableColumn>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {rows.map((row) => (
+          <TableRow key={row.key}>
+            {row.cells.map((cell, index) => (
+              <TableCell key={index}>{cell as React.ReactElement}</TableCell>
+            ))}
+            <TableCell key="action">
+              <Button size="sm">Visualizar</Button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </NextTable>
   );
 };
 
